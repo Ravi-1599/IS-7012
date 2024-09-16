5860ba6bfebfa95838fe526f1da16e544d0d0a41
Ravi-1599 <157281632+Ravi-1599@users.noreply.github.com>
2024-09-16 11:09:52 -0400

    Delete WEEK2/Models/JobTitle.cs

--- WEEK2/Models/JobTitle.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RAVIP1599.Models
-{
-    public class JobTitle
-    {
-        public int? JobTitleId { get; set; }
-        public string? Title { get; set; }
-        public string? Description { get; set; }
-    }
-}
